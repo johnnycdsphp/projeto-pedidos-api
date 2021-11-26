068d964e295fcc92876e42fcd58455eb0b5d11d5
Johnny Santos <johnny.firevendas@gmail.com>
2021-11-26 17:56:02 -0300

    Implementação do https

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ var privateKey  = fs.readFileSync('/var/lib/jelastic/keys/projeto-pedidos-api.je
 var certificate = fs.readFileSync('/var/lib/jelastic/keys/projeto-pedidos-api.jelastic.saveincloud.net.cer', 'utf8');
 
 var credentials = {key: privateKey, cert: certificate};
-var express = require('express');
-var app = express();
 
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
