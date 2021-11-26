23d4e781b31872d0572d3983516772ba127fc5df
Johnny Santos <johnny.firevendas@gmail.com>
2021-11-26 14:31:15 -0300

    Ajustes de compatibilidade de biblioteca

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+const fs = require("fs")
 const http = require('http')
 const app = require('./app')
 
 const port = process.env.PORT || 3000
-const server = http.createServer( app )
 
-server.listen( port )
+const options = {
+    key: fs.readFileSync("server.key"),
+    cert: fs.readFileSync("server.cert")
+};
+
+const server = http.createServer(options, app)
+
+server.listen(port)
